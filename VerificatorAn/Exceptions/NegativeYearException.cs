f24bc5be275e9f1f90f75ff3e1bbbe3ec35df8a2
Andrei-Mihnea <andrei.mihnea@student.tuiasi.ro>
2025-10-17 17:02:40 +0300

    Started implementation for year verifier

---
+++ VerificatorAn/Exceptions/NegativeYearException.cs
@@ -0,0 +1,10 @@
+﻿namespace VerificatorAn.Exceptions
+{
+    internal class NegativeYearException : Exception
+    {
+        public NegativeYearException(int year):base($"Introduce a valid year. The year ({year}) is not valid")
+        {
+        }
+
+    }
+}
---
+++ VerificatorAn/PublicHoliday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificatorAn
+{
+    public class PublicHoliday
+    {
+        string name {  get; set; }
+        public string date {  get; set; }
+
+    }
+}
